05e80eb814a28e40425524911e330a25935da463
Nathan Collins <nathancollins@Nathans-MBP.lan>
2023-09-05 15:00:38 +0100

    feat: new config

---
+++ lua/custom/plugins/alpha.lua
@@ -0,0 +1,34 @@
+return {
+  'goolord/alpha-nvim',
+  config = function()
+    local alpha = require 'alpha'
+    local dashboard = require 'alpha.themes.dashboard'
+    dashboard.section.header.val = {
+      [[ ________   ________  _________  ___  ___  ________  ________      ]],
+      [[|\   ___  \|\   __  \|\___   ___\\  \|\  \|\   __  \|\   ___  \    ]],
+      [[\ \  \\ \  \ \  \|\  \|___ \  \_\ \  \\\  \ \  \|\  \ \  \\ \  \   ]],
+      [[ \ \  \\ \  \ \   __  \   \ \  \ \ \   __  \ \   __  \ \  \\ \  \  ]],
+      [[  \ \  \\ \  \ \  \ \  \   \ \  \ \ \  \ \  \ \  \ \  \ \  \\ \  \ ]],
+      [[   \ \__\\ \__\ \__\ \__\   \ \__\ \ \__\ \__\ \__\ \__\ \__\\ \__\]],
+      [[    \|__| \|__|\|__|\|__|    \|__|  \|__|\|__|\|__|\|__|\|__| \|__|]],
+    }
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "📄  New File", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("f", "🔍  Find Files", ":Telescope find_files <CR>"),
+      dashboard.button("g", "🔦  Grep", ":Telescope live_grep <CR>"),
+      dashboard.button("l", "⚡︎  LazyGit", ":LazyGit <CR>"),
+      dashboard.button("s", "🔄  Restore Session", [[:lua require("persistence").load() <cr>]]),
+      dashboard.button("q", "❌  Quit NVIM", ":qa<CR>"),
+    }
+    local handle = io.popen('fortune')
+    local fortune = handle:read("*a")
+    handle:close()
+    dashboard.section.footer.val = fortune
+
+    dashboard.config.opts.noautocmd = true
+
+    vim.cmd [[autocmd User AlphaReady echo 'ready']]
+
+    alpha.setup(dashboard.config)
+  end
+}
---
+++ lua/custom/plugins/test.lua
@@ -0,0 +1,64 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "haydenmeade/neotest-jest",
+    "marilari88/neotest-vitest",
+  },
+  keys = {
+    {
+      '<leader>tt',
+      function()
+        require("neotest").run.run(vim.fn.expand("%"))
+      end,
+      desc = "Test File",
+    },
+    {
+      '<leader>tr',
+      function()
+        require("neotest").run.run()
+      end,
+      desc = "Run Nearest Test",
+    },
+    {
+      '<leader>ts',
+      function()
+        require("neotest").summary.toggle()
+      end,
+      desc = "Run Last Test",
+    },
+    {
+      "<leader>tl",
+      function()
+        require("neotest").run.run_last()
+      end,
+      desc = "Run Last Test",
+    },
+    {
+      "<leader>tL",
+      function()
+        require("neotest").run.run_last({ strategy = "dap" })
+      end,
+      desc = "Debug Last Test",
+    },
+    {
+      "<leader>tw",
+      "<cmd>lua require('neotest').run.run({ jestCommand = 'jest --watch ' })<cr>",
+      desc = "Run Watch",
+    },
+  },
+  config = function()
+    require('neotest').setup({
+      adapters = {
+        require('neotest-jest')({
+          jestCommand = "npm test --",
+          jestConfigFile = "custom.jest.config.ts",
+          env = { CI = true },
+          cwd = function(path)
+            return vim.fn.getcwd()
+          end,
+        }),
+        require("neotest-vitest")
+      }
+    })
+  end
+}
