dffc4f3d0010c08fd1928fa4493e8cad718488b5
Nathan Collins <nathancollins@Nathans-MacBook-Pro.local>
2023-10-31 13:46:43 +0000

    feat: updates

--- init.lua
+++ init.lua
@@ -191,7 +191,7 @@ require('lazy').setup({
       -- refer to the configuration section below
     }
   },
-  { "jmederosalvarado/roslyn.nvim" },
+  -- { "jmederosalvarado/roslyn.nvim" },
   {
     'ThePrimeagen/harpoon',
     dependencies = {
@@ -514,14 +514,14 @@ mason_lspconfig.setup_handlers {
   end
 }
 
-require("roslyn").setup({
-  dotnet_cmd = "dotnet",              -- this is the default
-  roslyn_version = "4.8.0-3.23475.7", -- this is the default
-  on_attach = on_attach,
-  capabilities = capabilities,
-  handlers = handlers
-})
-
+-- require("roslyn").setup({
+--   dotnet_cmd = "dotnet",              -- this is the default
+--   roslyn_version = "4.8.0-3.23475.7", -- this is the default
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   handlers = handlers
+-- })
+--
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
 local cmp = require 'cmp'
@@ -604,12 +604,12 @@ require 'colorizer'.setup()
 --harpoon
 vim.keymap.set('n', '<leader>h', '<Cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>', { desc = 'Toggle Harpoon' })
 vim.keymap.set('n', '<leader>H', '<Cmd>lua require("harpoon.mark").add_file()<CR>', { desc = 'Add Open File To Harpoon' })
-vim.keymap.set('n', '1', '<Cmd>lua require("harpoon.ui").nav_file(1)<CR>', { desc = 'Open File 1' })
-vim.keymap.set('n', '2', '<Cmd>lua require("harpoon.ui").nav_file(2)<CR>', { desc = 'Open File 2' })
-vim.keymap.set('n', '3', '<Cmd>lua require("harpoon.ui").nav_file(3)<CR>', { desc = 'Open File 3' })
-vim.keymap.set('n', '4', '<Cmd>lua require("harpoon.ui").nav_file(4)<CR>', { desc = 'Open File 4' })
-vim.keymap.set('n', '5', '<Cmd>lua require("harpoon.ui").nav_file(5)<CR>', { desc = 'Open File 5' })
-vim.keymap.set('n', '6', '<Cmd>lua require("harpoon.ui").nav_file(6)<CR>', { desc = 'Open File 6' })
-vim.keymap.set('n', '7', '<Cmd>lua require("harpoon.ui").nav_file(7)<CR>', { desc = 'Open File 7' })
-vim.keymap.set('n', '8', '<Cmd>lua require("harpoon.ui").nav_file(8)<CR>', { desc = 'Open File 8' })
-vim.keymap.set('n', '9', '<Cmd>lua require("harpoon.ui").nav_file(9)<CR>', { desc = 'Open File 9' })
+vim.keymap.set('n', '<leader>1', '<Cmd>lua require("harpoon.ui").nav_file(1)<CR>', { desc = 'Open File 1' })
+vim.keymap.set('n', '<leader>2', '<Cmd>lua require("harpoon.ui").nav_file(2)<CR>', { desc = 'Open File 2' })
+vim.keymap.set('n', '<leader>3', '<Cmd>lua require("harpoon.ui").nav_file(3)<CR>', { desc = 'Open File 3' })
+vim.keymap.set('n', '<leader>4', '<Cmd>lua require("harpoon.ui").nav_file(4)<CR>', { desc = 'Open File 4' })
+vim.keymap.set('n', '<leader>5', '<Cmd>lua require("harpoon.ui").nav_file(5)<CR>', { desc = 'Open File 5' })
+vim.keymap.set('n', '<leader>6', '<Cmd>lua require("harpoon.ui").nav_file(6)<CR>', { desc = 'Open File 6' })
+vim.keymap.set('n', '<leader>7', '<Cmd>lua require("harpoon.ui").nav_file(7)<CR>', { desc = 'Open File 7' })
+vim.keymap.set('n', '<leader>8', '<Cmd>lua require("harpoon.ui").nav_file(8)<CR>', { desc = 'Open File 8' })
+vim.keymap.set('n', '<leader>9', '<Cmd>lua require("harpoon.ui").nav_file(9)<CR>', { desc = 'Open File 9' })
--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -3,12 +3,16 @@ return {
   config = function()
     require("conform").setup({
       formatters_by_ft = {
+        cs = { "csharpier" },
         css = { "prettierd" },
         javascript = { "prettierd" },
+        json = { "prettierd" },
         lua = { "stylua" },
         markdown = { "prettierd" },
+        scss = { "prettierd" },
         sh = { "shfmt" },
         typescript = { "prettierd" },
+        xml = { "prettierd" },
       },
       format_on_save = {
         lsp_fallback = true,
