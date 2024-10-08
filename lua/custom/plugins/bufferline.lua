16d0122cbdc426ea6d844807a9630c687dd675d3
Nathan Collins <nathan.collins@cap.energy>
2024-10-08 07:45:45 +0100

    refactor: remove stuff

--- lua/custom/plugins/bufferline.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'akinsho/bufferline.nvim',
-  branch = 'main',
-  dependencies = { 'echasnovski/mini.icons', version = false },
-  config = function()
-    require('bufferline').setup()
-  end,
-}
